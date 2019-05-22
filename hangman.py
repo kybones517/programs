a3c3a36544e36149a4ab8c55c8acc90a014f1224
kybones517 <50665128+kybones517@users.noreply.github.com>
2019-05-22 14:06:21 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -22,5 +22,40 @@ def getWordLen():
     print(clue)
     break
    
- 
+getWordLen()
+
+while attempts > 0:
+    guess = r.choice(databank)
+    print("is the letter %s in your word?"% guess)
+    anwser = input(">>>: ")
+    if anwser == "y":
+        correct +=1
+        print("Found!")
+        print("How many occurrences?")
+        while(True):
+            try:
+                occur = int(input(">>>: "))
+                #updateClue(int(input("Index: ")))
+                for i in range(occur):
+                    x = int(input("Index:"))
+                    clue.remove(clue[x])
+                    clue.insert(x,guess)
+                
+            except (ValueError,IndexError):
+                print("invalid index")
+            else:
+                break
+        print(clue)
+    else:
+        attempts-=1
+        print(attempts)
+    if correct == len(clue):
+        print("I win!") 
+        break
+    databank.remove(guess)
+    
+    if attempts == 0:
+      print("I have Lost! :(")
+      print("You win!") 
+    
  
